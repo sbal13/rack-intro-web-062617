078617a5eef6366f1ccb9015c07286c53cdf78fc
sbal13 <sbal13@gmail.com>
2017-07-15 18:56:00 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,8 +2,13 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write message
     resp.finish
+  end
+
+
+  def message
+  	"Hello, my name is Steven"
   end
 
 end
